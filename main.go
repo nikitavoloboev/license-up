7d4b848bffd026ffb3f447dcec969a426dccd687
Nikita Voloboev <nikita.voloboev@icloud.com>
2017-11-30 16:24:39 +0100

    Change MIT

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func create(name string, site string) error {
 		defer fo.Close()
 
 		MIT := "MIT License \n\n" +
-			"Copyright (c) " + strconv.Itoa(year) + " " + name + ", " + site + "\n\n" +
+			"Copyright (c) " + strconv.Itoa(year) + " " + name + " <" + site + ">\n\n" +
 			"Permission is hereby granted, free of charge, to any person obtaining a copy \n" +
 			"of this software and associated documentation files (the \"Software\"), to deal \n" +
 			"in the Software without restriction, including without limitation the rights \n" +
