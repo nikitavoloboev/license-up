d6603395e970592baec5a9e62891b2710405a5aa
John Letey <john.letey@colorado.edu>
2019-02-25 19:57:05 +0000

    docs(license): regenerate license (#33)

--- mit.go
+++ mit.go
@@ -18,15 +18,15 @@ func mitCreateWithSite(name string, surname string, site string) error {
 	}
 	defer fo.Close()
 
-	MIT := "MIT License \n\n" +
+	MIT := "MIT License\n\n" +
 		"Copyright (c) " + strconv.Itoa(year) + "-present, " + name + " " + surname + " (" + site + ")\n\n" +
-		"Permission is hereby granted, free of charge, to any person obtaining a copy \n" +
-		"of this software and associated documentation files (the \"Software\"), to deal \n" +
-		"in the Software without restriction, including without limitation the rights \n" +
-		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell \n" +
-		"copies of the Software, and to permit persons to whom the Software is \n" +
-		"furnished to do so, subject to the following conditions: \n\n" +
-		"The above copyright notice and this permission notice shall be included in all \n" +
+		"Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+		"of this software and associated documentation files (the \"Software\"), to deal\n" +
+		"in the Software without restriction, including without limitation the rights\n" +
+		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+		"copies of the Software, and to permit persons to whom the Software is\n" +
+		"furnished to do so, subject to the following conditions:\n\n" +
+		"The above copyright notice and this permission notice shall be included in all\n" +
 		"copies or substantial portions of the Software.\n\n" +
 		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
 		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
@@ -50,15 +50,15 @@ func mitCreate(name string, surname string) error {
 	}
 	defer fo.Close()
 
-	MIT := "MIT License \n\n" +
+	MIT := "MIT License\n\n" +
 		"Copyright (c) " + strconv.Itoa(year) + "-present, " + name + " " + surname + "\n\n" +
-		"Permission is hereby granted, free of charge, to any person obtaining a copy \n" +
-		"of this software and associated documentation files (the \"Software\"), to deal \n" +
-		"in the Software without restriction, including without limitation the rights \n" +
-		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell \n" +
-		"copies of the Software, and to permit persons to whom the Software is \n" +
-		"furnished to do so, subject to the following conditions: \n\n" +
-		"The above copyright notice and this permission notice shall be included in all \n" +
+		"Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
+		"of this software and associated documentation files (the \"Software\"), to deal\n" +
+		"in the Software without restriction, including without limitation the rights\n" +
+		"to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
+		"copies of the Software, and to permit persons to whom the Software is\n" +
+		"furnished to do so, subject to the following conditions:\n\n" +
+		"The above copyright notice and this permission notice shall be included in all\n" +
 		"copies or substantial portions of the Software.\n\n" +
 		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
 		"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
