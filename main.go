d5619573150d0327b81c4df469e2a6a03d453b86
John Letey <john.letey@colorado.edu>
2019-03-02 21:17:02 +0000

    fix: rename `bsd` -> `bsd3` (#38)

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var (
 	bsd2         = app.Command("bsd2", "Create BSD 2-Clause license.")
 	bsd2Name     = bsd2.Arg("name", "Name of license holder.").Required().String()
 	bsd2Surname  = bsd2.Arg("surname", "Surname of license holder.").Required().String()
-	bsd3         = app.Command("bsd", "Create BSD 3-Clause license.")
+	bsd3         = app.Command("bsd3", "Create BSD 3-Clause license.")
 	bsd3Name     = bsd3.Arg("name", "Name of license holder.").Required().String()
 	bsd3Surname  = bsd3.Arg("surname", "Surname of license holder.").Required().String()
 	cc0          = app.Command("cc0", "Create CC0 license.")
